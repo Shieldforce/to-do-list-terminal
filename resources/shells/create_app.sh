e8f8ae670085b29056148f74ae843d6d0639e213
Alexandre Ferreira <shieldforce2@gmail.com>
2024-12-30 19:35:36 -0300

    correção git pull

--- resources/shells/create_app.sh
+++ resources/shells/create_app.sh
@@ -209,6 +209,8 @@ docker run --rm \
 #
 # ./vendor/bin/sail artisan migrate:fresh --seed
 
+bash ./vendor/shieldforce/scoob/scoob --type docker-remove laravel-php-fpm-8.4-8094
+
 bash ./vendor/shieldforce/scoob/scoob --type docker-laravel \
         --version 8.4 \
         --port 8094 \
@@ -217,8 +219,6 @@ bash ./vendor/shieldforce/scoob/scoob --type docker-laravel \
 
 info "Preparando para criar banco de dados! Aguarde são só 10 segundinhos..."
 sleep 10
-
-bash ./vendor/shieldforce/scoob/scoob --type docker-remove laravel-php-fpm-8.4-8094
 
 docker exec -it laravel-php-fpm-8.4-8094 mysql -e "create database to_do_list_terminal"
 docker exec -it laravel-php-fpm-8.4-8094 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION"
